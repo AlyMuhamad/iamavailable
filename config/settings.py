594b0d844a10a52cffe8d7fe4c38fac1427924bd
Aly Mohamed <alimoh0801@gmail.com>
2025-10-14 23:28:07 +0300

    add icons

--- config/settings.py
+++ config/settings.py
@@ -81,6 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
         'TEST_NAME': os.path.join(os.path.dirname(__file__), 'db.sqlite3'),
+        'HOST': 'iamavailable.pythonanywhere.com/',
     }
 }
 
