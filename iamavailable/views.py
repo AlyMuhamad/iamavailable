4c1202c97a08e02104824fcc7f06377676411ac4
Aly Mohamed <alimoh0801@gmail.com>
2025-10-21 13:37:16 +0300

    Add search feature

--- iamavailable/views.py
+++ iamavailable/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from .models import Job
+from django.db.models import Q
+from .models import Job, Tag
 from .forms import JobForm
 
 # Create your views here.
 
 def index(request):
+    search_query = ''
+    location_query = ''
+    
+    if request.GET.get('search_query'):
+        search_query = request.GET.get('search_query')
+    
+    if request.GET.get('location_query'):
+        location_query = request.GET.get('location_query')
+      
+    tags = Tag.objects.filter(name__icontains=search_query)
+    
     context = {
-        'jobs': Job.objects.all(),
+        'jobs': Job.objects.distinct().filter(
+            Q(title__icontains=search_query) | 
+            Q(description__icontains=search_query) |
+            Q(tags__in=tags) &
+            Q(location__icontains=location_query)
+            ),
+        'search_query': search_query,
+        'location_query': location_query
     }
 
     return render(request, 'iamavailable/index.html', context)
