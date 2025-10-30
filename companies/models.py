ed54669b1211ce3b83cdbd9a40656260226a32b0
Aly Mohamed <alimoh0801@gmail.com>
2025-10-30 12:27:54 +0300

    Uncomment out companies

--- companies/models.py
+++ companies/models.py
@@ -1,15 +1,15 @@
-# from django.db import models
-# from users.models import Profile
-# import uuid
+from django.db import models
+from users.models import Profile
+import uuid
 
-# # Create your models here.
-# class Company(models.Model):
-#     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-#     owner = models.OneToOneField(Profile, on_delete=models.CASCADE)
-#     created = models.DateTimeField(auto_now_add=True)
-#     name = models.CharField(max_length=100)
-#     location = models.CharField(max_length=100)
-#     description = models.TextField()
+# Create your models here.
+class Company(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    owner = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    description = models.TextField()
     
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
