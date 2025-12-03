de36a9d7fe77e7ecfc279af0e96b7de18e6274f6
Aly Mohamed <alimoh0801@gmail.com>
2025-12-03 07:43:20 +0200

    Turn debug on

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.getenv('DEBUG')
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','iamavailable.pythonanywhere.com', '3.215.142.179', 'iamavailable.net', 'www.iamavailable.net']
 
